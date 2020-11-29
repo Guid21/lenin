bc25ebcc8e8ae396bae186e8636d5976f76c6748
guid21 <uryuk.uz@gmail.com>
2020-11-29 10:49:07 +0300

    upd

--- src/secns/Papers/components/ListPapers/ListPapers.tsx
+++ src/secns/Papers/components/ListPapers/ListPapers.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { List, Spin, Tag, Typography } from 'antd';
 
 import ListPaperContainer from '../../../../containers/ListPaperContainer';
